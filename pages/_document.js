c2f84c0a2e06fbba59ea8229ff3c88cc7c699302
Shafiqullah Turkmen <shafiqullahturkmen@gmail.com>
2023-10-07 18:59:04 +0300

    Banner styling added

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-      <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@700&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
--- pages/components/banner/banner.jsx
+++ pages/components/banner/banner.jsx
@@ -1,19 +1,41 @@
+import Image from "next/image";
 import styles from "./banner.module.css";
 
-const Banner = ({title, subTitle, imgUrl}) => {
-
+const Banner = ({ title, subTitle, imgUrl }) => {
   const handleOnPlay = () => {
     console.log("Button clicked");
-  }
+  };
 
   return (
-    <div>
-      <h3>{title}</h3>
-      <h3>{subTitle}</h3>
-      <button onClick={handleOnPlay}>Play</button>
-      <div className={styles.bannerBackgroundImage} style={{ backgroundImage: `url(${imgUrl})`}}></div>
+    <div className={styles.container}>
+      <div className={styles.leftWrapper}>
+        <div className={styles.left}>
+          <div className={styles.nseriesWrapper}>
+            <p className={styles.firstLetter}>N</p>
+            <p className={styles.series}>S E R I E S</p>
+          </div>
+          <h3 className={styles.title}>{title}</h3>
+          <h3 className={styles.subTitle}>{subTitle}</h3>
+
+          <div className={styles.playButtonWrapper}>
+            <button className={styles.btnWithIcon} onClick={handleOnPlay}>
+              <Image
+                src="/static/play-arrow.svg"
+                alt="play arrow"
+                width="32"
+                height="32"
+              />
+              <span className={styles.playText}>Play</span>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div
+        className={`${styles.bannerBackgroundImage} ${styles.bannerImg}`}
+        style={{ backgroundImage: `url(${imgUrl})` }}
+      ></div>
     </div>
-  )
-}
- 
-export default Banner
+  );
+};
+
+export default Banner;
