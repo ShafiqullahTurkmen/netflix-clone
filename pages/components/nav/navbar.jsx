70c76dbf795841dacbeba67eebd1f36de955ece9
Shafiqullah Turkmen <shafiqullahturkmen@gmail.com>
2023-10-08 15:51:11 +0300

    navbar component styling added

--- pages/components/nav/navbar.jsx
+++ pages/components/nav/navbar.jsx
@@ -1,24 +1,28 @@
-import React from "react";
+import styles from "./navbar.module.css";
 
 const Navbar = ({ username }) => {
   return (
-    <div>
-      Navbar
-      <p>{username}</p>
-      <ul>
-        <li>Home</li>
-        <li>My List</li>
-      </ul>
-      <nav>
-        <div>
-          <button>
-            <p>{username}</p>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <a className={styles.logoLink}>
+          <div className={styles.logoWrapper}>Netflix</div>
+        </a>
+        <ul className={styles.navItems}>
+          <li className={styles.navItem}>Home</li>
+          <li className={styles.navItem2}>My List</li>
+        </ul>
+        <nav className={styles.navContainer}>
+          <button className={styles.usernameBtn}>
+            <p className={styles.username}>{username}</p>
           </button>
-          <div>
-            <a>Sign out</a>
+          <div className={styles.navDropdown}>
+            <div>
+              <a className={styles.linkName}>Sign out</a>
+              <div className={styles.lineWrapper}></div>
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
     </div>
   );
 };
