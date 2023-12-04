12277b2f303a9ae056b5143410fef69bb4368c19
Shafiqullah Turkmen <shafiqullahturkmen@gmail.com>
2023-12-04 09:22:04 +0300

    Card Component Image Error Handler added

--- pages/components/card/card.jsx
+++ pages/components/card/card.jsx
@@ -1,17 +1,32 @@
 import Image from "next/image";
 import styles from "./card.module.css";
-const Card = ({ imgUrl, size }) => {
+import { useState } from "react";
+const Card = ({ imgUrl, size = "medium" }) => {
+  const [imgSrc, setImgSrc] = useState(imgUrl);
+
+
   const classMap = {
-    "large": styles.lgItem,
-    "medium": styles.mdItem,
-    "small": styles.smItem,
+    large: styles.lgItem,
+    medium: styles.mdItem,
+    small: styles.smItem,
   };
+
+  const handleOnError = (e) => {
+    console.log(e, e.message);
+    setImgSrc("/static/clifford.webp");
+  }
 
   return (
     <div className={styles.container}>
       Card
       <div className={classMap[size]}>
-        <Image src={imgUrl} alt="card image" layout="fill" className={styles.cardImg}/>
+        <Image
+          src={imgSrc}
+          alt="card image"
+          layout="fill"
+          className={styles.cardImg}
+          onError={handleOnError}
+        />
       </div>
     </div>
   );
