dacfcb2c5e172cc6a23141b6e1f4badfc634dcd8
Shafiqullah Turkmen <shafiqullahturkmen@gmail.com>
2023-12-08 04:41:03 +0300

    Sign page structure added

---
+++ pages/login/index.jsx
@@ -0,0 +1,53 @@
+import Head from "next/head";
+import styles from "./index.module.css";
+import Link from "next/link";
+import Image from "next/image";
+
+const Login = () => {
+  const handleLoginWithEmail = (e) => {
+    e.preventDefault();
+    console.log("it worksssssssssssss");
+  };
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Netflix SignIn</title>
+      </Head>
+
+      <header>
+        <div className={styles.headerWrapper}>
+          <Link href="/" className={styles.logoLink}>
+            <div className={styles.logoWrapper}>
+              <Image
+                src="/static/netflix.svg"
+                alt="Netflix Login Image"
+                width="128"
+                height="34"
+              />
+            </div>
+          </Link>
+        </div>
+
+        <main className={styles.main}>
+          <div className={styles.mainWrapper}>
+            <h1 className={styles.signinHeader}>Sign In</h1>
+
+            <input
+              type="text"
+              placeholder="Email address"
+              className={styles.emailInput}
+            />
+
+            <p className={styles.userMsg}></p>
+            <button onClick={handleLoginWithEmail} className={styles.loginBtn}>
+              Sign In
+            </button>
+          </div>
+        </main>
+      </header>
+    </div>
+  );
+};
+
+export default Login;
